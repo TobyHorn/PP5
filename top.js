5f06aea7212653d4ed0891eba005e436f747c595
TobyHorn <kankuro865@gmail.com>
2018-11-11 11:11:56 -0600

    Update top.js

--- top.js
+++ top.js
@@ -66,7 +66,7 @@ class Controller {
     getTop(e) {
         
         //Create variables
-        const baseStr = "https://jikan.me/api/anime/";
+        const baseStr = "https://jikan.moe/api/anime/";
         let counter = 1;
         let animeId;
         let loops = 0;
